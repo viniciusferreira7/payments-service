1f53c6f501691e06cb67c3b96e0bb23bcdbe0ab0
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-08-15 08:25:33 -0300

    feat(payment-service): add payment service on events module

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentQueueService } from './payment-queue/payment-queue.service';
 import { RabbitmqService } from './rabbitmq/rabbitmq.service';
+import { PaymentServiceService } from './payment-service/payment-service.service';
 
 @Module({
-  providers: [RabbitmqService, PaymentQueueService],
+  providers: [RabbitmqService, PaymentQueueService, PaymentServiceService],
   exports: [PaymentQueueService],
 })
 export class EventsModule {}
