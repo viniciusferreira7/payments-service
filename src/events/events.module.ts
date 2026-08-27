354f7eb115565021a5b65902a09b6f10673d785e
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-08-27 10:51:55 -0300

    feat(dlq-controller): register the dlq controller in the events module

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { DlqController } from './dlq/dlq.controller';
 import { DlqService } from './dlq/dlq.service';
 import { PaymentQueueService } from './payment-queue/payment-queue.service';
 import { PaymentConsumerService } from './payment-service/payment-consumer.service';
 import { RabbitmqService } from './rabbitmq/rabbitmq.service';
 
 @Module({
+  controllers: [DlqController],
   providers: [
     RabbitmqService,
     PaymentQueueService,
