41520f0f1ed709409ad6b6fd6541b9345f8ff9ac
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-08-29 11:54:11 -0300

    feat(dlq.controller): create stats route

--- src/events/dlq/dlq.controller.ts
+++ src/events/dlq/dlq.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { getErrorDetails } from '@/utils/error.util';
+import { DlqService } from './dlq.service';
+import type { DLQStats } from './interfaces/dlq-stats.interface';
 
 @Controller('dlq')
-export class DlqController {}
+export class DlqController {
+  private readonly logger = new Logger(DlqController.name);
+
+  constructor(private readonly dlqService: DlqService) {}
+
+  @Get('/stats')
+  public async getStats(): Promise<DLQStats> {
+    try {
+      return this.dlqService.getStats();
+    } catch (error) {
+      const errorDetails = getErrorDetails(error);
+
+      this.logger.error(
+        `Failed to get DLQ: ${errorDetails.message}`,
+        errorDetails.stack
+      );
+
+      throw new InternalServerErrorException(
+        errorDetails,
+        'Failed to get DLQ stats'
+      );
+    }
+  }
+}
