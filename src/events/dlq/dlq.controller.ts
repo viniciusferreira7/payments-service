a2d48b4d7a1396b0220bbbe9f4cd82dc15335c5b
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-08-29 12:00:41 -0300

    feat(dlq.controller): create messages route

--- src/events/dlq/dlq.controller.ts
+++ src/events/dlq/dlq.controller.ts
@@ -3,9 +3,11 @@ import {
   Get,
   InternalServerErrorException,
   Logger,
+  Query,
 } from '@nestjs/common';
 import { getErrorDetails } from '@/utils/error.util';
 import { DlqService } from './dlq.service';
+import type { DLQMessage } from './interfaces/dlq-message.interface';
 import type { DLQStats } from './interfaces/dlq-stats.interface';
 
 @Controller('dlq')
@@ -23,6 +25,36 @@ export class DlqController {
 
       this.logger.error(
         `Failed to get DLQ: ${errorDetails.message}`,
+        errorDetails.stack
+      );
+
+      throw new InternalServerErrorException(
+        errorDetails,
+        'Failed to get DLQ stats'
+      );
+    }
+  }
+
+  @Get('/messages')
+  public async getMessages(
+    @Query('limit') limit?: string
+  ): Promise<{ count: number; messages: DLQMessage[] }> {
+    try {
+      const parsedLimit = limit ? parseInt(limit, 10) : 10;
+
+      const messages = await this.dlqService.peekMessages(
+        Math.abs(parsedLimit)
+      );
+
+      return {
+        count: messages.length,
+        messages,
+      };
+    } catch (error) {
+      const errorDetails = getErrorDetails(error);
+
+      this.logger.error(
+        `Failed to get DLQ messages: ${errorDetails.message}`,
         errorDetails.stack
       );
 
